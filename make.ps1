a374fcba7ee815aaa21e94c86ed715d1692dff66
Kevin Vu <kevin.m.vu@gmail.com>
2018-03-22 20:34:00 -0700

    Make build commands more concise

--- make.ps1
+++ make.ps1
@@ -9,8 +9,7 @@ if ($args.length -gt 0 -and $args -eq 'clean')
 
 $toolchainPrefix = 'gcc-arm-none-eabi-7-2017-q4-major-win32\bin\arm-none-eabi-'
 
-& "$($toolchainPrefix)as" -march=armv6 -o setup_for_c.o setup_for_c.s
-& "$($toolchainPrefix)gcc" -c -march=armv6 kernel.c -o kernel.o
-& "$($toolchainPrefix)ld" -T link.ld -o kernel.elf setup_for_c.o kernel.o
+& "$($toolchainPrefix)gcc" -c -march=armv6 *.s *.c
+& "$($toolchainPrefix)ld" -T link.ld -o kernel.elf *.o
 & "$($toolchainPrefix)objdump" -D kernel.elf > kernel.elf.list
 & "$($toolchainPrefix)objcopy" -O binary kernel.elf kernel.img
