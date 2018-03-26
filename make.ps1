00e7c1c858c228ced1d68c92ab8ee169f138608c
Kevin Vu <kevin.m.vu@gmail.com>
2018-03-25 19:28:50 -0700

    Enable gcc warnings

--- make.ps1
+++ make.ps1
@@ -9,7 +9,8 @@ if ($args.length -gt 0 -and $args -eq 'clean')
 
 $toolchainPrefix = 'gcc-arm-none-eabi-7-2017-q4-major-win32\bin\arm-none-eabi-'
 
-& "$($toolchainPrefix)gcc" -ffreestanding -c -march=armv6 *.s *.c
+# TODO: enable pointer-sign warning
+& "$($toolchainPrefix)gcc" -Wno-pointer-sign -Wall -ffreestanding -c -march=armv6 *.s *.c
 & "$($toolchainPrefix)ld" -T link.ld -o kernel.elf *.o
 & "$($toolchainPrefix)objdump" -D kernel.elf > kernel.elf.list
 & "$($toolchainPrefix)objcopy" -O binary kernel.elf kernel.img
