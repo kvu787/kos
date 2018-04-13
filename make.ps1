22f7e80f7effe8474d72aa188b8a1211bd4a996b
Kevin Vu <kevin.m.vu@gmail.com>
2018-04-13 13:23:11 -0700

    Run clean before building to avoid linking in old object files

--- make.ps1
+++ make.ps1
@@ -1,14 +1,19 @@
-if ($args.length -gt 0 -and $args -eq 'clean')
-{
+function Clean {
     rm *.o
     rm *.list
     rm *.img
     rm *.elf
+}
+
+if ($args.length -gt 0 -and $args[0] -eq 'clean')
+{
+    Clean
     exit
 }
 
 $toolchainPrefix = 'gcc-arm-none-eabi-7-2017-q4-major-win32\bin\arm-none-eabi-'
 
+Clean
 # TODO: enable pointer-sign warning
 & "$($toolchainPrefix)gcc" -Wno-pointer-sign -Wall -ffreestanding -c -march=armv6 *.s *.c
 & "$($toolchainPrefix)ld" -L'gcc-arm-none-eabi-7-2017-q4-major-win32/arm-none-eabi/lib' -T link.ld -o kernel.elf *.o -l'c'
