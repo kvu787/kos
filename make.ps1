ede8cc7d76d7149016c504106b6bb83cab0e6a7b
Kevin Vu <kevin.m.vu@gmail.com>
2018-04-13 15:36:42 -0700

    Remove TODO that has been done

--- make.ps1
+++ make.ps1
@@ -14,7 +14,6 @@ if ($args.length -gt 0 -and $args[0] -eq 'clean')
 $toolchainPrefix = 'gcc-arm-none-eabi-7-2017-q4-major-win32\bin\arm-none-eabi-'
 
 Clean
-# TODO: enable pointer-sign warning
 & "$($toolchainPrefix)gcc" -O0 -Wpedantic -Wall -std=c11 -ffreestanding -c -march=armv6 *.s *.c
 & "$($toolchainPrefix)ld" -L'gcc-arm-none-eabi-7-2017-q4-major-win32/arm-none-eabi/lib' -T link.ld -o kernel.elf *.o -l'c'
 & "$($toolchainPrefix)objdump" -D kernel.elf > kernel.elf.list
