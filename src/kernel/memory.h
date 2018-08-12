f1f7ccba4f93aa3c088d155bd511d05d8713da42
Kevin Vu <kevin.m.vu@gmail.com>
2018-08-11 22:06:35 -0700

    Add comments

--- src/kernel/memory.h
+++ src/kernel/memory.h
@@ -3,8 +3,13 @@
 
 #include "types.h"
 
+// Returns true iff size bytes starting at a and b are equal.
 bool_t memory_equals(void *a, void *b, uint_t size);
+
+// Set size bytes beginning at start to value.
 void memory_set(void *start, uint_t size, byte_t value);
+
+// Copy size bytes from src dst.
 void memory_copy(void *src, void *dst, uint_t size);
 
 #endif // MEMORY_H_
