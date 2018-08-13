798266be39372484cc3ae6ab5f79b5873faada82
Kevin Vu <kevin.m.vu@gmail.com>
2018-08-12 20:16:46 -0700

    Add comments

--- src/kernel/char.h
+++ src/kernel/char.h
@@ -3,8 +3,14 @@
 
 #include "types.h"
 
+// Returns true iff c is a space or tab.
 bool_t is_whitespace(char_t c);
+
+// Returns true iff c is a digit (0-9).
 bool_t is_digit(char_t c);
+
+// Returns true iff c can be displayed in a cell, which means c is any ASCII
+// character from '!' to '~'.
 bool_t is_printable(char_t c);
 
 #endif // CHAR_H_
